6ab69bdbf7d5e885f5f7458d9540313a0db1966d
toejamhoney <toejamhoney@gmail.com>
2014-07-15 11:08:51 -0400

    Cleaned up code and added method to return value based on xml tag

--- build_pdf_objects.py
+++ build_pdf_objects.py
@@ -1,71 +1,82 @@
-import lxml.etree
+import lxml.etree, re
+from util import unescapeHTMLEntities
 
-#search tree for specified tag with specified value
-def search_tree (root, tag):
-    for key in root.iterfind(tag):
-           return key
-    else: 
+#Determine the type of tag used and return its value accordingly
+def get_value (elem, root):
+    if elem.tag == "literal" or elem.tag == "number":
+        return unescapeHTMLEntities(elem.text)
+    elif elem.tag == "string":
+        return unescapeHTMLEntities(elem.text.decode('base64'))
+    elif elem.tag == "ref":
+        #find the referenced object and return its value
+        obj = get_ref_object(elem.get('id'), root)
+        return get_value(obj[0], root)
+    elif elem.tag == "stream":
+        return unescapeHTMLEntities(elem[1].text.decode('base64'))
+    elif elem.tag == "dict":
+        #build the dictionary
+        ret = {}
+        size = elem.get("size")
+        size = re.sub("%", "", size)
+        dict_elems = elem.getchildren()
+        for i in range(int(size)):
+            val = get_value(dict_elems[i][0], root)
+            if val is not None:
+                ret[dict_elems[i].tag] = val
+    else:
+        #some tags not accounted for: Rect, field, xfa, Media, etc
+        ret = None
+    return ret
+    
+
+#find the object referenced in another object
+def get_ref_object(id, root):
+    for obj in root.iterfind(".//object"):
+        if obj.get("id") == id:
+            return obj
+    else:
         return None
 
+#Get any annotation objects in the PDF and store in the app object
 def get_annots(app, root):
-  for annot in root.iterfind(".//Annots"):
-    objs = []
-    parent = annot.getparent()
-    ref_list = parent[parent.index(annot)+1][0]
-    for ref in ref_list:
-      id = ref.get("id")
-      for obj in root.iterfind(".//object"):
-          if obj.get("id") == id:
-              size = obj[0].get("size")
-              size = re.sub("%", "", size)
-              new = {}
-              childs =  obj[0].getchildren()
-              for i in range(int(size)):
-                  if childs[2*i+1][0].tag == "literal":
-                      new[childs[2*i].text] = unescapeHTMLEntities(childs[2*i+1][0].text)
-                  elif childs[2*i+1][0].tag == "ref":
-                      for ob in root.iterfind(".//object"):
-                          if ob.get("id") == childs[2*i+1][0].get("id"):
-                              for child in ob.iterdescendants(tag="data"):
-                                 new[childs[2*i].text] = unescapeHTMLEntities(child.text.decode('base64'))
-                  else:
-                       new[childs[2*i].text] = "Unknown tag: " + childs[2*i+1][0].tag
-              new["subject"] = new.pop("Subj")
-              app['doc']['annots'].append(new)
+    for annot in root.iterfind(".//Annots"):
+      annot_list = annot[0]
+      for ref in annot_list:
+        id = ref.get("id")
+        obj = get_ref_object(id, root)
+        new = get_value(obj[0], root)
+        if new is not None:
+            new["subject"] = new.pop("Subj")
+            app['doc']['annots'].append(new)
 
+
+#Mimic the Adobe event object by parsing the PDF for commonly found attributes
 def create_event_obj(tree):
     event_attrs = ["author", "calculate", "creator", "creationDate", "delay", "dirty", "external", "filesize", "keywords", "modDate", "numFields", "numPages", "numTemplates", "path", "pageNum", "producer", "subject", "title", "zoom", "zoomType"]
     event = {}
     event["target"] ={}
     for item in event_attrs:
-        elem = search_tree(tree, item[0].upper() + item[1:])
-        if elem != None:
-            parent = elem.getparent()
-            sibling = parent[parent.index(elem)+1][0]
-            if sibling.tag == "string" and sibling.text != None:
-                event["target"][item] = unescapeHTMLEntities(sibling.text.decode('base64'))
-            elif sibling.tag == "ref":
-                for ob in tree.iterfind(".//object"):
-                    if ob.get("id") == sibling.get("id"):
-                        for child in ob.iterdescendants(tag="data"):
-                            if child.text != None:
-                                event["target"][item] = unescapeHTMLEntities(child.text.decode('base64'))
-            else:
-                event["target"][item] = "Unknown tag: " + sibling.tag
+        for elem in tree.iterfind('.//' + item[0].upper() + item[1:]):
+            val = get_value(elem[0], tree)
+            if val is not None:
+                event["target"][item] = val
     #print event
     return event
 
+#Mimic the Adobe app object by parsing the PDF for commonly found attributes
 def create_app_obj(tree):
     app= {}
     app_attrs = ["calculate", "formsVersion", "fullscreen", "language", "numPlugins", "openInPlace", "platform", "toolbar", "toolbarHorizontal", "toolbarVertical"]
     doc = {}
     for item in app_attrs:
-        elem = search_tree(tree, item[0].upper() + item[1:])
-        if elem != None:
-            parent = elem.getparent()
-            doc[item] = unescapeHTMLEntities(parent[parent.index(elem)+1][0].text)
+        for elem in tree.iterfind('.//' + item[0].upper() + item[1:]):
+            val = get_value(elem[0], tree)
+            if val is not None:
+                doc[item] = val
     app['doc'] = doc;
-    app['doc']['annots'] = []
+
+    #Many app values are dependent on the reader
+    #set some common defaults here
     app['doc']['viewerType'] = 'Reader'
     app['viewerType'] = 'Reader'
     app['viewerVersion'] = 5.0
@@ -74,27 +85,22 @@ def create_app_obj(tree):
         app['language'] = "ENU"
     if not 'platform' in app.keys():
         app['platform'] = "WIN"
+
+    #store the annotation objects so they can be retrieved later
+    app['doc']['annots'] = []
     get_annots(app, tree)
+    #print app
     return app
 
+#Mimic the Adobe info object by parsing the PDF for commonly found attributes
 def create_info_obj(tree):
     info_attrs = ["author", "creator", "creationDate", "Date", "keywords", "modDate", "producer", "subject", "title", "trapped"]
-    this = {}
-    this["info"] ={}
+    info = {}
     for item in info_attrs:
-        elem = search_tree(tree, item[0].upper() + item[1:])
-        if elem != None:
-            parent = elem.getparent()
-            sibling = parent[parent.index(elem)+1][0]
-            if sibling.tag == "string" and sibling.text != None:
-                this["info"][item] = unescapeHTMLEntities(sibling.text.decode('base64'))
-            elif sibling.tag == "ref":
-                for ob in tree.iterfind(".//object"):
-                    if ob.get("id") == sibling.get("id"):
-                        for child in ob.iterdescendants(tag="data"):
-                            this["info"][item] = unescapeHTMLEntities(child.text.decode('base64'))
-            else:
-                this["info"][item] = "Unknown tag: " + sibling.tag
-    #print this
-    return this
+        for elem in tree.iterfind('.//' + item[0].upper() + item[1:]):
+            val = get_value(elem[0], tree)
+            if val is not None:
+                info[item] = val
+    #print info
+    return info
 
