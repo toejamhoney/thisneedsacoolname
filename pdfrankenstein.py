c93439f95b19608da1250d31c2ebfcf6df174927
Rosencrantz <toejamhoney@gmail.com>
2014-07-21 12:40:01 -0400

    Added sdhashes to js and de_js. No error checking.

--- pdfrankenstein.py
+++ pdfrankenstein.py
@@ -7,7 +7,9 @@ import time
 import getopt
 import hashlib
 import argparse
+import tempfile
 import traceback
+import subprocess
 import multiprocessing
 from Queue import Full, Empty
 
@@ -156,7 +158,7 @@ class Hasher(multiprocessing.Process):
             t_hash = ''
             t_str = ''
             graph = ''
-            js = ''
+            obf_js = ''
             de_js = ''
             swf = ''
             fsize = ''
@@ -188,9 +190,11 @@ class Hasher(multiprocessing.Process):
                     graph = self.make_graph(parsed_pdf, err)
                     t_str = self.make_tree_string(parsed_pdf, err)
                     t_hash = self.make_tree_hash(graph, err)
-                    js = self.get_js(parsed_pdf, err)
-                    de_js = self.get_deobf_js(js, parsed_pdf, err)
-                    urls = self.get_urls(js, err)
+                    obf_js = self.get_js(parsed_pdf, err)
+                    de_js = self.get_deobf_js(obf_js, parsed_pdf, err)
+                    obf_js_sdhash = self.make_sdhash(obf_js, err)
+                    de_js_sdhash = self.make_sdhash(de_js, err)
+                    urls = self.get_urls(obf_js, err)
                     urls += self.get_urls(de_js, err)
                     swf = self.get_swf(parsed_pdf, err)
                     bin_blob = parsed_pdf.bin_blob
@@ -207,13 +211,15 @@ class Hasher(multiprocessing.Process):
                     'pdf_md5':pdf_name,
                     'tree_md5':t_hash,
                     'tree':t_str,
-                    'obf_js':js,
+                    'obf_js':obf_js,
                     'deobf_js':de_js,
                     'swf':swf,
                     'graph':graph,
                     'pdfsize':pdfsize,
                     'urls':urls,
                     'bin_blob':bin_blob,
+                    'obf_js_sdhash':obf_js_sdhash,
+                    'de_js_sdhash':de_js_sdhash,
                     'error':err })
             self.counter.inc()
             self.qin.task_done()
@@ -244,6 +250,21 @@ class Hasher(multiprocessing.Process):
         except TypeError:
             err.append('<HashException>%s</HashException>' % traceback.format_exc())
         return t_hash
+
+    def make_sdhash(self, data, err=''):
+        try:
+            tmpfile = tempfile.NamedTemporaryFile(delete=True)
+        except IOError:
+            stdout = ''
+        else:
+            tmpfile.write(data)
+            cmd = ['sdhash', tmpfile.name]
+            proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = proc.communicate(data)
+            tmpfile.close()
+        finally:
+            return stdout
+
     def get_js(self, pdf, err=''):
         return 'Hasher: Unimplemented method, %s' % sys._getframe().f_code.co_name
     def get_debof_js(self, js, pdf, err=''):
@@ -499,7 +520,9 @@ class DbStorage(Storage):
         'tree',
         'graph',
         'obf_js',
-        'deobf_js',
+        'de_js',
+        'obf_js_sdhash',
+        'de_js_sdhash',
         'swf',
         'abc',
         'actionscript',
